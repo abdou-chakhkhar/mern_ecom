4cd66b6b7a98eed1cd1e53f04baf6b8334064b15
abdou30.cia <abdou30.cia@gmail.com>
2021-05-03 22:00:45 +0000

    a forgeting commit

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -19,8 +19,12 @@ router.get('/', asyncHandler(async(req, res) => {
 router.get('/:id', asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
     
-    if(product) res.json(product)
-    else res.status(404).json({ message: 'Product not found'})
+    if(product) {
+        res.json(product)
+    } else {
+        res.status(404)
+        throw new Error('Product not found')
+    }
 }))
 
 export default router;
