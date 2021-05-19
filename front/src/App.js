63a448eaf9d00edd560473f16c1aa49b03730bde
abdou30.cia <abdou30.cia@gmail.com>
2021-05-19 14:08:05 +0100

    User login reducer & action

--- front/src/App.js
+++ front/src/App.js
@@ -14,9 +14,10 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <Route path='/' component={HomeScreen} exact />
+          <Route path='/login' component={LoginScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
       <Footer />
