968b992a919859d4c259505eed6a47787d8b044a
abdou30.cia <abdou30.cia@gmail.com>
2021-05-27 13:39:00 +0100

    User profile; problem to fix

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,4 +77,33 @@ const getUserProfile = asyncHandler(async (req, res) => {
     }
 })
 
-export { authUser, registerUser, getUserProfile }
+//@desc Update user profile
+//@route GET /api/users/profile
+//@access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if(user){
+        user.name = req.body.name || user.name
+        user.email = req.body.email || userr.email
+        if(req.body.password){
+            user.password = req.body.password
+        }
+
+        const updateUser = await user.save()
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+            token: generateToken(updateUser._id)
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+
+})
+
+export { authUser, registerUser, getUserProfile, updateUserProfile }
