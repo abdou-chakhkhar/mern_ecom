b5ecce7ab1b1edaf941949e4b5cab84c45d5803e
abdou30.cia <abdou30.cia@gmail.com>
2021-05-28 23:39:52 +0100

    order controller & route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 import { addOrderItems } from '../controllers/orderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
-
+// hhjg
 
 router.route('/').post(protect, addOrderItems)
 
