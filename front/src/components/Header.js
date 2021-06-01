cc3701f898a832e493a4d9d1a5a570c3bb9f5048
abdou30.cia <abdou30.cia@gmail.com>
2021-06-01 18:05:03 +0100

    admin user list

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -49,7 +49,7 @@ const Header = () => {
                 </LinkContainer>
                 )
               }
-                            {userInfo && userInfo.isAdmin && (
+              {userInfo && userInfo.isAdmin && (
                 <NavDropdown title='Admin' id='adminmenu'>
                   <LinkContainer to='/admin/userlist'>
                     <NavDropdown.Item>Users</NavDropdown.Item>
