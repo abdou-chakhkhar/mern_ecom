daaa05ee16f65760e955dbf66b2d25358574636f
abdou30.cia <abdou30.cia@gmail.com>
2021-06-01 17:44:08 +0100

    User details and orders reset

--- front/src/actions/userActions.js
+++ front/src/actions/userActions.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
+import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
 import {
   USER_DETAILS_FAIL,
   USER_DETAILS_REQUEST,
+  USER_DETAILS_RESET,
   USER_DETAILS_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
@@ -54,6 +56,8 @@ export const register = (name, email, password) => async (dispatch) => {
     dispatch({
       type: USER_REGISTER_REQUEST,
     })
+    dispatch({ type: USER_DETAILS_RESET })
+    dispatch({ type: ORDER_LIST_MY_RESET })
     const config = {
       headers: {
         'Content-Type': 'application/json',
